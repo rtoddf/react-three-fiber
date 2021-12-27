36ad88a2647bf280e7d1d9f37aa4ca2ef77a99fe
rtoddf <rtoddf@me.com>
2021-12-27 13:16:03 -0500

    initial commit for trying react-three-fiber

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react';
+
+import { Canvas, useFrame } from '@react-three/fiber';
+
+import './App.scss';
+
+const Box = () => {
+  const mesh = useRef(null);
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+
+  return (
+    <mesh ref={mesh}>
+      {/* height, width,and depth */}
+      <boxBufferGeometry attach='geometry' args={[1,1,1]} />
+      <meshStandardMaterial attach='material' />
+    </mesh>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Canvas>
+        <Box />
+      </Canvas>
+    </>
   );
 }
 
 export default App;
+
+// https://www.youtube.com/watch?v=fdtqqyeKRJk&t=2433s
